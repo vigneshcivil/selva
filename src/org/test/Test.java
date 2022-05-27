025540f3254b4eff02ba4a77047b1e7975870609
vigneshcivil <jayamvicky1997@gmail.com>
2022-05-27 15:26:06 +0530

    selva time

---
+++ src/org/test/Test.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class Test {
+	
+	private void test1() {
+		System.out.println("1");
+
+	}
+	private void test2() {
+		System.out.println("2");
+
+	}
+	private void test3() {
+		System.out.println("3");
+
+	}
+
+
+
+}
